faab9f34a568b03a03adc326f4f727941edfe0ea
Ahmad Nur <ahmadnursahid12345@gmail.com>
2025-06-19 10:28:10 +0700

    feat: Add Token and Auth models with token management functionality

---
+++ lib/app/constants/api_constants.dart
@@ -0,0 +1,24 @@
+import 'package:ora_news/app/utils/token_manager.dart';
+
+class ApiConstants {
+  static const String baseUrl = 'https://ora-news.vercel.app/api/v1';
+  static const String loginEndpoint = '/auth/login';
+  static const String registerEndpoint = '/auth/register';
+  static const String forgotPasswordEndpoint = '/auth/forgot-password';
+
+  static Future<Map<String, String>> get authHeaders async {
+    var token = await TokenManager.getTokens();
+
+    if (token == null) {
+      return {'Content-Type': 'application/json'};
+    }
+    return {
+      'Content-Type': 'application/json',
+      'Authorization': '${token.tokenType} ${token.accessToken}',
+    };
+  }
+
+  static Map<String, String> get headers {
+    return {'Content-Type': 'application/json'};
+  }
+}
---
+++ lib/data/models/auth_models.dart
@@ -0,0 +1,70 @@
+// TOKEN
+class Token {
+  final String accessToken;
+  final String tokenType;
+
+  Token({required this.accessToken, required this.tokenType});
+
+  Token copyWith({String? accessToken, String? tokenType}) => Token(
+    accessToken: accessToken ?? this.accessToken,
+    tokenType: tokenType ?? this.tokenType,
+  );
+
+  factory Token.fromJson(Map<String, dynamic> json) =>
+      Token(accessToken: json["accessToken"], tokenType: json["tokenType"]);
+
+  Map<String, dynamic> toJson() => {"accessToken": accessToken, "tokenType": tokenType};
+}
+
+// LOGIN MODEL
+class Login {
+  final String username;
+  final String password;
+
+  Login({required this.username, required this.password});
+
+  Login copyWith({String? username, String? password}) =>
+      Login(username: username ?? this.username, password: password ?? this.password);
+
+  factory Login.fromJson(Map<String, dynamic> json) =>
+      Login(username: json["username"], password: json["password"]);
+
+  Map<String, dynamic> toJson() => {"username": username, "password": password};
+}
+
+// REGISTER MODEL
+class Register {
+  final String email;
+  final String username;
+  final String name;
+  final String password;
+
+  Register({
+    required this.email,
+    required this.username,
+    required this.name,
+    required this.password,
+  });
+
+  Register copyWith({String? email, String? username, String? name, String? password}) =>
+      Register(
+        email: email ?? this.email,
+        username: username ?? this.username,
+        name: name ?? this.name,
+        password: password ?? this.password,
+      );
+
+  factory Register.fromJson(Map<String, dynamic> json) => Register(
+    email: json["email"],
+    username: json["username"],
+    name: json["name"],
+    password: json["password"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "email": email,
+    "username": username,
+    "name": name,
+    "password": password,
+  };
+}
